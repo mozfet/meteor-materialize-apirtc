6ed4f20b51fcd9c8be5c61b1c5c5830fad7c1a1a
Mozfet <wynand@expertbox.com>
2018-11-21 17:31:08 +0200

    upgrade to v4 api

--- package/package.js
+++ package/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mozfet:materialize-apirtc',
   summary: 'MaterializeCSS styled WebRTC Implementation using ApiRTC for Meteor.',
-  version: '0.0.4',
+  version: '1.0.0',
   git: 'https://github.com/mozfet/meteor-materialize-apirtc.git'
 });
 
